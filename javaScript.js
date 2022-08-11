7b5433912c83c39159bc1ef28a49c41cba6c70ac
petromamboubee <gm.boubee@gmail.com>
2022-08-11 17:32:48 -0300

    Fiz até o requisito 10

--- javaScript.js
+++ javaScript.js
@@ -2,9 +2,9 @@ const login = document.getElementById('login');
 const email = document.getElementById('email');
 const password = document.getElementById('senha');
 
-function verificarLogin () {
-    if(email.value == 'tryber@teste.com' && password.value == '123456') {
+function verificarLogin() {
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
-    } else alert('Email ou senha inválidos.'); 
+  } else alert('Email ou senha inválidos.');
 }
-login.addEventListener('click', verificarLogin)
+login.addEventListener('click', verificarLogin);
